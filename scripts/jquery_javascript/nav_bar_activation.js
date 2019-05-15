92264edd3282450cc6f329cbe8710ced831a5226
Samuil <samuil2001ivanov@gmail.com>
2019-05-15 21:38:00 +0300

    Changing the size and font of the text.

--- scripts/jquery_javascript/nav_bar_activation.js
+++ scripts/jquery_javascript/nav_bar_activation.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
     var path = window.location.href;
-    console.log(this.location.href);
-    console.log("path: " + path);
-    console.log(path == this.location.href);
      $('li a').each(function() {
         if($(this).attr('href').indexOf(path) !== -1){
             $(this).parent().addClass('active');
